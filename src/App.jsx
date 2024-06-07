33f37179d183178bfb9feb2121c86d16562ed924
Pedro Zeferino da Silva <zefpedro@hotmail.com>
2024-06-07 16:01:14 -0300

    first commit com o Header e o Footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import "./index.css"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
